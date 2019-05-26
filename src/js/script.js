ef952a576c5e99bcd4edeb6e012a4f17b74cf9d6
Kseniia Moiseeva <mailtoksena@gmail.com>
2019-05-26 14:47:34 +0300

    music

--- src/js/script.js
+++ src/js/script.js
@@ -30,35 +30,81 @@ $(document).ready(function() {
 
 //mp3 treck
 
-// обработчик нажатия на кнопку плей
-$('.music__player button').click(function () {
-	var parent = $(this).parent();
-	
-	var button = $(this);
-	var audio = $('audio', parent)[0];
-	var duration = $('.music__player--duration', parent);
-	
-	// переключение состояния плеера и смена картинки на кнопке - плей или пауза //'url(/images/play.png)'   'url(/images/pause.png)' 
-	if (audio.paused == false) {
-		audio.pause();
-    button.css('background', 'url(../img/play-button.png)');
-    button.css('background-size', 'cover');
-	} else {
-		audio.play();
-		button.css('background', 'url(../img/pause-button.svg)');
-	}
-	
-	// по окончанию трека на кнопку устанавливается картинка плей
-	$(audio).on('ended', function() {
-		button.css('background', 'url(../img/play-button.svg)');
-	});
-	
-	// обновление текущей позиции воспроизведения
-	$(audio).on('timeupdate', function() {
-		var date = new Date(audio.currentTime * 1000);
-		duration.html(date.getMinutes()+':'+date.getSeconds());
-	});
+
+
+
+/////
+
+(function($) {
+	$(document).ready(function() {
+
+
+  // обработчик нажатия на кнопку плей
+  $('.music__player button').click(function () {
+    var parent = $(this).parent();
+    
+    var button = $(this);
+    var audio = $('audio', parent)[0];
+    var duration = $('.music__player--duration', parent);
+    
+    // переключение состояния плеера и смена картинки на кнопке - плей или пауза //'url(/images/play.png)'   'url(/images/pause.png)' 
+    if (audio.paused == false) {
+      audio.pause();
+      button.css('background', 'url(../img/play-button.png)');
+      button.css('background-size', 'cover');
+    } else {
+      audio.play();
+      button.css('background', 'url(img/pause-button.svg)');
+    }  
+  });
+
+  // по окончанию трека на кнопку устанавливается картинка плей
+$('.music__player audio').on('ended', function() {
+    var player = $(this).parent();
+    var button = $('button', player);
+    button.css('background', 'url(img/play-button.svg)');
+  });
+
+
+// обновление текущей позиции воспроизведения
+$('.music__player audio').on('timeupdate', function() {
+  
+  var audio = this;
+  var player = $(this).parent();
+  var duration = $('.music__player--duration', player);
+  
+  var date = new Date(audio.currentTime * 1000);
+  duration.html(date.getMinutes()+':'+date.getSeconds());
+  
+
+  var progress =  $('.progressbar', player);
+  if(progress && !progress.attr('data-sliding'))
+  {
+    $(progress).slider( "value", audio.currentTime / audio.duration * 100);
+  }
+
+  
 });
 
+$('.music__player .progressbar').slider({
+  value: 0,
+  stop: function( event, ui ) {
+   
+    var parent = $(this).parent();
+    var audio = $('audio', parent)[0];
+
+    audio.currentTime = ui.value * audio.duration / 100;
+
+    $(this).attr('data-sliding', null);
+  }, 
+  start: function( event, ui ) {
+    $(this).attr('data-sliding', 'true');
+  }
+});
+
+
+    
+	});
+})(jQuery);
 
 
