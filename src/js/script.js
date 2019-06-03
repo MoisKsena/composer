7dcd50973fc8f01bee294fb61b0f44a5db16cdbd
Kseniia Moiseeva <mailtoksena@gmail.com>
2019-06-04 01:27:16 +0300

    show textblock

--- src/js/script.js
+++ src/js/script.js
@@ -18,6 +18,46 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
+//show textblock
+
+$("#about__content-text").fadeIn(5000);
+
+
+//autoType
+
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "block"
+  });
+ // thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  //thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+}
+$(document).ready(function(){
+  // Now to start autoTyping just call the autoType function with the 
+  // class of outer div
+  // The second paramter is the speed between each letter is typed.   
+  autoType(".type-js",100);
+});
+
 //modal open
 
 function showPopup() {
